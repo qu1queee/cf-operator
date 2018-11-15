3402b75d43f2925fff97c925b27c174487449bce
Andre Duffeck <aduffeck@users.noreply.github.com>
2018-11-15 10:22:41 +0100

    Revert "cmd kube switches"

--- cmd/root.go
+++ cmd/root.go
@@ -56,11 +56,14 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringP("kubeconfig", "c", "", "Path to a kubeconfig, not required in-cluster")
+	rootCmd.PersistentFlags().StringP("master", "m", "", "Kubernetes API server address")
 	rootCmd.PersistentFlags().StringP("namespace", "n", "default", "Namespace to watch for BOSH deployments")
 	viper.BindPFlag("kubeconfig", rootCmd.PersistentFlags().Lookup("kubeconfig"))
+	viper.BindPFlag("masterURL", rootCmd.PersistentFlags().Lookup("master"))
 	viper.BindPFlag("namespace", rootCmd.PersistentFlags().Lookup("namespace"))
 	viper.BindEnv("kubeconfig")
 	viper.BindEnv("namespace", "CFO_NAMESPACE")
+	viper.BindEnv("masterURL", "CFO_MASTER_URL")
 }
 
 // initConfig is executed before running commands
@@ -74,9 +77,10 @@ func initConfig() {
 
 func getKubeConfig() (*rest.Config, error) {
 	kubeconfig := viper.GetString("kubeconfig")
+	masterURL := viper.GetString("masterURL")
 
 	if len(kubeconfig) > 0 {
-		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+		return clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
 	}
 
 	// If no explicit location, try the in-cluster config
